06a5551658fb7dfc709f6c255a67825ed07df925
mh bakhshipour <mh.bakhshipour@yahoo.com>
2023-03-22 16:23:03 +0000

    add __str__ for user model for more readablity

--- client/models.py
+++ client/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=11, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.username}"
